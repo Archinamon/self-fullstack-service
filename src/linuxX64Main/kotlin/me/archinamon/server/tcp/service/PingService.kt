b09ce15661520d39cd9b70ff5fe71d5efc276e8c
archinamon <archinamon@yandex.ru>
2020-11-05 23:47:57 +0300

    implement common service codegen backend

--- src/linuxX64Main/kotlin/me/archinamon/server/tcp/service/PingService.kt
+++ src/linuxX64Main/kotlin/me/archinamon/server/tcp/service/PingService.kt
@@ -2,20 +2,10 @@ package me.archinamon.server.tcp.service
 
 import me.archinamon.server.tcp.bind.BinderService
 import me.archinamon.server.tcp.bind.NativeObjectBinder
-import me.archinamon.server.tcp.bind.RouteCommand
-import me.archinamon.server.tcp.bind.TcpSocketBinder
-
-//todo: codegen all this file with kompiler:plugin
 
 @NativeObjectBinder(PingServiceImpl::class)
-object PingServiceImpl : BinderService(), PingService {
+actual object PingServiceImpl : BinderService(), PingService {
     override suspend fun ping(): String {
         return "OK"
-    }
-}
-
-object PingServiceBinder : TcpSocketBinder<PingServiceImpl>(PingServiceImpl::class) {
-    init {
-        bind(RouteCommand("ping"), PingService::ping)
     }
 }
